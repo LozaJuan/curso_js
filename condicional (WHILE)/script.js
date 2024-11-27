07a5b6a9a644cc3ebfaabaac3565be1cfb0df8f1
juanloza <juan.loza@gmx.com>
2024-11-28 00:00:07 +0100

    practicando el while

--- condicional (WHILE)/script.js
+++ condicional (WHILE)/script.js
@@ -27,12 +27,54 @@ while (z>=0) {document.write('El número es: ', z);
 document.write('El bucle ha terminado');
 */
 
+/*
 let x = 1;
 let suma = 0;
 let valor;
 while (x<=5) { 
     valor = parseInt(prompt('INTRODUZCA SU VALOR'));
-    suma = suma + valor;
+    suma = valor + suma;
     x++;
 }
 document.write('EL RESULTADO DE LA SUMA ES: ', suma, '<br>');
+*/
+
+/*
+let x;
+x = 1;
+let suma;
+suma = 0;
+let valor;
+while (x<=5) {
+    valor = parseInt(prompt('Introduzca valor:'));
+    suma= valor + suma;
+    x++
+}
+document.write('El resultado de la suma es:', suma,'<br>');
+*/
+
+/*
+let x;
+x = 1;
+let suma;
+suma = 0 ;
+let valor;
+while (x<=2) {
+    valor = parseInt(prompt('Vamos hacer unas sumas que numeros vas a elegir?'));
+    suma = valor + suma;
+    x++;
+}
+document.write('El resultado de la suma es:', suma);
+*/
+
+let x;
+x=1;
+let valor;
+let suma;
+suma = 0;
+while (x<=2) {
+    valor = parseInt(prompt('Vamos a ver si la multiplcacion funciona'));
+    suma = suma + valor;
+    x++;
+}
+document.write('digamos que este es el resultado:', suma);
